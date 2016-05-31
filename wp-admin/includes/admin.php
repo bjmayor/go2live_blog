770f948cefb16d7a2a87b182dc0a963bc43ac734
maynard <shunping.liu@huangjinqianbao.com>
2016-05-31 13:35:45 +0800

    使用wordpress4.3.1.1448

--- wp-admin/includes/admin.php
+++ wp-admin/includes/admin.php
@@ -6,6 +6,18 @@
  * @subpackage Administration
  */
 
+if ( ! defined('WP_ADMIN') ) {
+	/*
+	 * This file is being included from a file other than wp-admin/admin.php, so
+	 * some setup was skipped. Make sure the admin message catalog is loaded since
+	 * load_default_textdomain() will not have done so in this context.
+	 */
+	load_textdomain( 'default', WP_LANG_DIR . '/admin-' . get_locale() . '.mo' );
+}
+
+/** WordPress Administration Hooks */
+require_once(ABSPATH . 'wp-admin/includes/admin-filters.php');
+
 /** WordPress Bookmark Administration API */
 require_once(ABSPATH . 'wp-admin/includes/bookmark.php');
 
@@ -33,6 +45,9 @@ require_once(ABSPATH . 'wp-admin/includes/plugin.php');
 /** WordPress Post Administration API */
 require_once(ABSPATH . 'wp-admin/includes/post.php');
 
+/** WordPress Administration Screen API */
+require_once(ABSPATH . 'wp-admin/includes/screen.php');
+
 /** WordPress Taxonomy Administration API */
 require_once(ABSPATH . 'wp-admin/includes/taxonomy.php');
 
@@ -49,16 +64,18 @@ require_once(ABSPATH . 'wp-admin/includes/theme.php');
 /** WordPress User Administration API */
 require_once(ABSPATH . 'wp-admin/includes/user.php');
 
+/** WordPress Site Icon API */
+require_once(ABSPATH . 'wp-admin/includes/class-wp-site-icon.php');
+
 /** WordPress Update Administration API */
 require_once(ABSPATH . 'wp-admin/includes/update.php');
 
 /** WordPress Deprecated Administration API */
 require_once(ABSPATH . 'wp-admin/includes/deprecated.php');
 
-/** WordPress Multi-Site support API */
+/** WordPress Multisite support API */
 if ( is_multisite() ) {
+	require_once(ABSPATH . 'wp-admin/includes/ms-admin-filters.php');
 	require_once(ABSPATH . 'wp-admin/includes/ms.php');
 	require_once(ABSPATH . 'wp-admin/includes/ms-deprecated.php');
 }
-
-?>
